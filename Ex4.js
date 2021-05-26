7f85705c47e84253a1b2f036cf5b27f5f4939a18
Gokul Sankar <gokul.sankar29@gmail.com>
2021-05-27 00:35:37 +0530

    NEW COMMIT2

--- Ex4.js
+++
@@ -1,8 +0,0 @@
-var repeat = true;
-while(repeat){
-	var reply = prompt("Are we there yet?");
-	if(reply.toLowerCase() === "yes" || reply.toLowerCase() === "yeah" || reply.toLowerCase().indexOf("yes") != -1){
-		repeat = false;
-		alert("Yayyyyyy!");
-	}
-}
